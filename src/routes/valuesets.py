eface948c6f3c1d740cd7093a8635bce1b271f24
Tyler Madonna <tjm159@pitt.edu>
2026-04-29 13:12:53 -0400

    Removed auth from get valuesets

--- src/routes/valuesets.py
+++ src/routes/valuesets.py
@@ -1,13 +1,11 @@
 from flask import Blueprint, request
 
 from common.database.valuesets import find_valuesets
-from common.decorator import require_globus_groups_token
 
 valuesets_bp = Blueprint("valuesets", __name__)
 
 
 @valuesets_bp.route("/valuesets", methods=["GET"])
-@require_globus_groups_token(required_group_name="senotype-edit")
 def get_valuesets():
     # get predicate_term from query parameters
     predicate_term = request.args.get("predicate_term")
